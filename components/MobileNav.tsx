7d801f0bdbc37b951802c8cba1c4f668c6a82796
suranjit9 <suranjitm07@gmail.com>
2024-04-08 17:39:01 -0400

    auth

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,11 +1,77 @@
-
+"use client";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { siderBarLink } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const MobileNav = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const pathname = usePathname();
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Image
+            src="/icons/hamburger.svg"
+            alt={"hamburger"}
+            width={36}
+            height={36}
+            className="cursor-pointer sm:hidden"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="bg-dark-1 border-none">
+          <Link href={"/"} className="flex items-center gap-1">
+            <Image
+              src={"/icons/logo.svg"}
+              width={32}
+              height={32}
+              alt={"logo"}
+              className="max-sm:size-10"
+            />
+            <p className="text-[26px] font-extrabold text-white ">
+              Meet <span className="text-blue-1">Pro</span>
+            </p>
+          </Link>
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <section className="flex h-full items-start flex-col gap-6 pt-16 text-white">
+                {siderBarLink.map((link) => {
+                  const isActive =
+                    pathname === link.route ;
+                  return (
+                    <SheetClose asChild key={link.route}>
+                        <Link
+                      href={link.route}
+                      key={link.label}
+                      className={cn(
+                        `flex gap-4 items-center p-4 rounded-lg w-full max-w-60`,
+                        { "bg-blue-1": isActive }
+                      )}
+                    >
+                      <Image
+                        src={link.imgUrl}
+                        alt={link.label}
+                        width={20}
+                        height={20}
+                      />
+                      <p className="text-sm font-semibold ">{link.label}</p>
+                    </Link>
+                    </SheetClose>
+                  );
+                })}
+              </section>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
 };
 
 export default MobileNav;
--- components/SiderBar.tsx
+++ components/SiderBar.tsx
@@ -8,11 +8,11 @@ import { usePathname } from "next/navigation";
 const SiderBar = () => {
   const pathname = usePathname();
   return (
-    <section className=" static left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
-      <div className=" flex flex-col gap-6">
+    <section className=" static left-0  top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className=" flex flex-col gap-6 items-start">
         {siderBarLink.map((link) => {
           const isActive =
-            pathname === link.route || pathname.startsWith(link.route);
+            pathname === link.route || pathname.startsWith(`${link.route}/`);
           return (
             <Link
               href={link.route}
